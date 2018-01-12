f017d8c77e2056dc9fd825d07583c0035c2a5648
Taylor Lassiter <lassit09@gmail.com>
2018-01-12 03:09:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
+
+
 function deleteFromObjectByKey(object, key) {
-  delete object.key;
+
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+
 }
