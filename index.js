4dbdca232421b4fe4a13bfa861268d31fd894da6
Taylor Lassiter <lassit09@gmail.com>
2018-01-12 02:56:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = new Object({});
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({recipes}, object, {[key]: value});
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object.key = value;
+}
